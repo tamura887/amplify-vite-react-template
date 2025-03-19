9a459a5638b102b084e47acf371e795e626cc6c3
ap746277 <tamu2007>
2025-03-19 13:45:31 +0900

    iroiro34

--- amplify/functions/resource.ts
+++ amplify/functions/resource.ts
@@ -1,12 +1,10 @@
 import { defineFunction } from '@aws-amplify/backend';
 
 export const functions = defineFunction({
-  functionName: 'createEntryData', // Correct property name
-  handler: 'src/functions/createEntryData.handler',
+  name: 'createEntryData', // Correct property name
+  entry: 'src/functions/createEntryData.ts',
   environment: {
     TABLE_NAME: process.env.TABLE_NAME || 'entrydata',
   },
-  permissions: {
-    'dynamodb:PutItem': ['arn:aws:dynamodb:region:account-id:table/entrydata'],
-  },
+  
 });
