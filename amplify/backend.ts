36f4b7c8643794366944617399f62fe66da7b28e
ap746277 <tamu2007>
2025-03-19 11:25:45 +0900

    iroiro12

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,10 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
+import { functions } from './functions/resource'; // Add this line
 
 defineBackend({
   auth,
   data,
+  functions, // Add this line
 });
